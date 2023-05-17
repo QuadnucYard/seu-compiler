13adda4d94dd60efd427a25db91b91bbdff67d80
QuadnucYard <2380433991@qq.com>
2023-05-17 15:26:00 +0800

    Fix outfmt

--- include/utils/outfmt.hpp
+++ include/utils/outfmt.hpp
@@ -11,25 +11,25 @@ namespace qy {
 		unsigned line_size{10};
 		unsigned field_width{6};
 		unsigned indent{4};
+		bool with_brace{true};
 	};
 
 	template <std::ranges::input_range R>
 	std::string format_array(R&& r, format_array_args args = {}) {
+		// This format string need to be prolonged
+		auto fstr = fmt::format("{}{{:{}}},\n", std::string(args.indent, ' '), args.field_width);
+		auto ff = fmt::runtime(fstr);
 		std::string s;
-		auto ff = fmt::runtime(
-			args.indent > 0 ? fmt::format("{{1:{}}}{{0:{}}},\n", args.indent, args.field_width)
-							: fmt::format("{{0:{}}},\n", args.field_width));
 		for (auto&& chk : r | tl::views::chunk(args.line_size))
-			s += fmt::format(ff, fmt::join(chk, ","), "");
-		return fmt::format("{{\n{}}}", s);
+			s += fmt::format(ff, fmt::join(chk, ","));
+		return args.with_brace ? fmt::format("{{\n{}}}", s) : s;
 	}
 
 	template <std::ranges::input_range R>
 	std::string format_array_2d(R&& r, format_array_args args = {}) {
+		auto fstr = fmt::format("{}{{:{}}},\n", std::string(args.indent, ' '), args.field_width);
+		auto ff = fmt::runtime(fstr);
 		std::string s;
-		auto ff = fmt::runtime(
-			args.indent > 0 ? fmt::format("{{1:{}}}{{0:{}}},\n", args.indent, args.field_width)
-							: fmt::format("{{0:{}}},\n", args.field_width));
 		/* auto ff2 = fmt::runtime(
 		args.indent > 0 ? 	fmt::format("{{1:{}}}{{{{\n{{2:{}}}{{0}}{{3:{}}}}}}},\n" ,
 						args.indent, args.indent, args.indent)
