5aa7761c53e31363cf388817d8997b6615249fc8
QuadnucYard <2380433991@qq.com>
2023-05-18 09:51:56 +0800

    Add conditional statements to templater

--- examples/templater.cpp
+++ examples/templater.cpp
@@ -14,14 +14,58 @@ int main(int argc, char const* argv[]) {
 	cout << temp.str() << endl;
 
 	// It can be also constructed from path.
-	qy::templater temp2{"E:\\Project\\seu-compiler\\examples\\aaa.c"};
+	/* qy::templater temp2{"E:\\Project\\seu-compiler\\examples\\aaa.c"};
 	temp2.set_string("[[anything]]", "char cccc = 'a';");
-	cout << temp2.str() << endl;
+	cout << temp2.str() << endl; */
 
 	// Use auto-linked variable
 	extern const char* TEMPLATE;
 	// auto xx = std::string{TEMPLATE};
 	std::ofstream out("test.out.cpp");
 	out << TEMPLATE;
+
+	/* qy::templater temp3{R"(======
+		0000
+		[[IF(cond)]]
+			1111
+			[[IF(cond2)]]
+			6666
+			[[FI]]
+			2222
+		[[FI]]
+		000
+		[[IF(cond)]]
+			3333
+			[[IF(cond2)]]
+			4444
+			[[FI]]
+			5555
+		[[FI]]
+		00
+=====)"}; */
+	qy::templater temp3{R"(======
+		0000
+		[[IF(cond)]]
+			1111
+		[[ELSE]]
+			1000
+		[[FI]]
+			6666
+		[[IF(cond2)]]
+			2222
+		[[FI]]
+		000
+		[[IF(cond)]]
+			3333
+		[[FI]]	
+			4444
+		[[IF(cond2)]]
+			5555
+		[[FI]]
+		00
+=====)"};
+	temp3.set_bool("cond2", true);
+	temp3.set_bool("cond", false);
+	cout << temp3.str() << endl;
 	return 0;
 }
--- include/utils/templater.hpp
+++ include/utils/templater.hpp
@@ -1,7 +1,17 @@
 #pragma once
 #include "fs.hpp"
+#include "string_utils.hpp"
 #include <fstream>
+#include <regex>
 #include <sstream>
+
+#if __has_include(<format>)
+	#include <format>
+	#define FORMAT std::format
+#else
+	#include <fmt/core.h>
+	#define FORMAT fmt::format
+#endif
 
 namespace qy {
 	class templater {
@@ -10,32 +20,72 @@ namespace qy {
 			if (fs::exists(string_or_path))
 				load(string_or_path);
 			else
-				temp = string_or_path;
+				tmpl = string_or_path;
 		}
 
 		void set_string(std::string_view key, std::string_view value) {
-			auto p = temp.find(key);
-			if (p != std::string_view::npos)
-				temp.replace(p, key.length(), value);
+			replace_all_inplace(tmpl, key, value);
 		}
 
-		const std::string& str() const {
-			return temp;
+		void set_bool(std::string_view key, bool value) {
+			using namespace std::string_literals;
+			if (value) {
+				std::regex re{FORMAT(
+					R"(\[\[IF\({}\)\]\]([\s\S]*?)\[\[ELSE\]\]([\s\S]*?)\[\[FI\]\])", key)};
+				tmpl = std::regex_replace(tmpl, re, "$1");
+				std::regex re2{FORMAT(R"(\[\[IF\({}\)\]\]([\s\S]*?)\[\[FI\]\])", key)};
+				tmpl = std::regex_replace(tmpl, re2, "$1");
+			} else {
+				std::regex re{FORMAT(
+					R"(\[\[IF\({}\)\]\]([\s\S]*?)\[\[ELSE\]\]([\s\S]*?)\[\[FI\]\])", key)};
+				tmpl = std::regex_replace(tmpl, re, "$2");
+				std::regex re2{FORMAT(R"(\[\[IF\({}\)\]\]([\s\S]*?)\[\[FI\]\])", key)};
+				tmpl = std::regex_replace(tmpl, re2, "");
+			}
+			return;
+
+			if (value) {
+				auto k_if_v = FORMAT("[[IF({})]]", key);
+				auto k_if = "[[IF("s;
+				auto k_fi = "[[FI]]"s;
+				size_t g_scan = 0;
+				while (true) {
+					auto p = tmpl.find(k_if_v, g_scan);
+					if (p == std::string::npos)
+						break;
+					auto p_scan = p + k_if_v.length();
+					size_t p2 = p;
+					for (int cnt = 1; cnt > 0;) {
+						auto p1 = tmpl.find(k_if, p_scan);
+						p2 = tmpl.find(k_fi, p_scan);
+						if (p1 != std::string::npos && p1 < p2) {
+							cnt++;
+							p_scan = p1 + k_if.length();
+						} else {
+							cnt--;
+						}
+					}
+					tmpl.replace(p, p2 + k_fi.length() + 1, "");
+					g_scan = p;
+				}
+			}
 		}
+
+		const std::string& str() const { return tmpl; }
 
 		void load(const fs::path& path) {
 			std::ifstream fin(path);
 			std::ostringstream iss;
 			iss << fin.rdbuf();
-			temp = iss.str();
+			tmpl = iss.str();
 		}
 
 		void dump(const fs::path& path) const {
 			std::ofstream out(path);
-			out << temp;
+			out << tmpl;
 		}
 
 	private:
-		std::string temp;
+		std::string tmpl;
 	};
 } // namespace qy
