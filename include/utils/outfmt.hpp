0ec96c0c3d1a6fe680640fa00c5680a335d1fac4
QuadnucYard <2380433991@qq.com>
2023-06-02 18:07:34 +0800

    Simplify outfmt

--- include/utils/outfmt.hpp
+++ include/utils/outfmt.hpp
@@ -16,32 +16,22 @@ namespace qy {
 
 	template <std::ranges::input_range R>
 	std::string format_array(R&& r, format_array_args args = {}) {
-		// This format string need to be prolonged
-		auto fstr = fmt::format("{}{{:{}}},\n", std::string(args.indent, ' '), args.field_width);
-		auto ff = fmt::runtime(fstr);
 		std::string s;
 		for (auto&& chk : r | tl::views::chunk(args.line_size))
-			s += fmt::format(ff, fmt::join(chk, ","));
+			s += fmt::format("{:<{}}{:>{}}\n", "", args.indent, fmt::join(chk, ","),
+							 args.field_width);
 		return args.with_brace ? fmt::format("{{\n{}}}", s) : s;
 	}
 
 	template <std::ranges::input_range R>
 	std::string format_array_2d(R&& r, format_array_args args = {}) {
-		auto fstr = fmt::format("{}{{:{}}},\n", std::string(args.indent, ' '), args.field_width);
-		auto ff = fmt::runtime(fstr);
 		std::string s;
-		/* auto ff2 = fmt::runtime(
-		args.indent > 0 ? 	fmt::format("{{1:{}}}{{{{\n{{2:{}}}{{0}}{{3:{}}}}}}},\n" ,
-						args.indent, args.indent, args.indent)
-						: fmt::format("{{{{\n{{0}}}}}},\n")
-						); */
-		// auto ff2 = fmt::runtime(fmt::sprintf("{:%d}{\n{{}}{:%d}},\n", args.indent, args.indent));
 		for (auto&& a : r) {
 			std::string ss;
 			for (auto&& chk : a | tl::views::chunk(args.line_size))
-				ss += fmt::format(ff, fmt::join(chk, ","), "");
-			// s += fmt::format(ff2, ss, "", "", "");
-			s += fmt::format("{{\n{}}},\n", ss);
+				ss += fmt::format("{:<{}}{:>{}}\n", "", args.indent, fmt::join(chk, ","),
+								  args.field_width);
+			s += fmt::format("{:<{}}{{\n{}{:<{}}}},\n", "", args.indent, ss, "", args.indent);
 		}
 		return fmt::format("{{\n{}}}", s);
 	}
